629d9c0cff4795d24235933821c42d35be4f4763
RG <117524772+ziguin0925@users.noreply.github.com>
2024-06-12 17:13:39 +0900

    [level 1] Title: 모의고사, Time: 0.86 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#) 
 
 ### 성능 요약
 
-메모리: 75.1 MB, 시간: 9.02 ms
+메모리: 69.3 MB, 시간: 0.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 12일 17:11:03
+2024년 06월 12일 17:13:37
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,45 +1,30 @@
+
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
+    public static int[] solution(int[] answers) {
+        int[][] patterns = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
 
-        ArrayList<Score> Student = new ArrayList<>();
-
-        Student.add(new Score(new int[] {1,2,3,4,5}));
-        Student.add(new Score(new int[] {2,1,2,3,2,4,2,5}));
-        Student.add(new Score(new int[] {3,3,1,1,2,2,4,4,5,5}));
-
-        int length = answers.length;
-        for(int i=0; i<length; i++){
-            for(Score an : Student){
-                if(answers[i]==an.number[i% an.number.length]){
-                    an.score++;
-                }
+        int[] hit = new int[3];
+        for(int i = 0; i < hit.length; i++) {
+            for(int j = 0; j < answers.length; j++) {
+                if(patterns[i][j % patterns[i].length] == answers[j]) hit[i]++;
             }
         }
 
-        int[] arr = {Student.get(0).score, Student.get(1).score, Student.get(2).score};
-        List<Integer> a = new ArrayList<>();
-        int k = Arrays.stream(arr).max().getAsInt();
-        for(int i=0; i<Student.size(); i++){
-            if(k==Student.get(i).score){
-                a.add(i+1);
-            }
+        int max = Math.max(hit[0], Math.max(hit[1], hit[2]));
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < hit.length; i++)
+            if(max == hit[i]) list.add(i + 1);
 
-        }
-        int[] answer= a.stream().mapToInt(Integer::intValue).toArray();
-
-        return a.stream().mapToInt(Integer::intValue).toArray();
-
-
-    }
-}
-
-class Score{
-    int score=0;
-    int[] number;
-
-    public Score(int[] number){
-        this.number = number;
+        int[] answer = new int[list.size()];
+        int cnt = 0;
+        for(int num : list)
+            answer[cnt++] = num;
+        return answer;
     }
 }

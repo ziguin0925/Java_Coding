0ad783bf7cfb30a18696f05e5aad080753af2ac9
RG <117524772+ziguin0925@users.noreply.github.com>
2024-06-15 18:22:02 +0900

    [level 1] Title: 실패율, Time: 0.41 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -3,17 +3,15 @@ class Solution {
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
         double[] arr = new double[N+2];
-        int[] k= Arrays.stream(stages).distinct().sorted().toArray();
-
         ArrayList<double[]> list = new ArrayList<>(N);
 
         for (int i = 0; i < stages.length; i++) {//각 스테이지에 도전하고 있는 사람들.
             arr[stages[i]]++;
         }
 
-        
+
         double size =stages.length;
-        
+
         for(int i =0; i<arr.length; i++){
             if(arr[i]!=0){
                 double num =arr[i];
@@ -22,11 +20,6 @@ class Solution {
             }
         }
 
-
-        //check : 스테이지 별 실패율(0이 아닌)
-        // k : 실패율이 0이 아닌 스테이지
-
-       
 
         for(int i = 1; i < N+1; i++) {
             list.add(new double[]{i,arr[i]});

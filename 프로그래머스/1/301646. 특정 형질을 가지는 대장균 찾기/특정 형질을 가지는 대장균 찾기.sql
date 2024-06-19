541cd982284353dd28638799994b23dda5bc1aeb
RG <117524772+ziguin0925@users.noreply.github.com>
2024-06-19 18:21:01 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,6 +1,6 @@
 -- 코드를 작성해주세요
 SELECT COUNT(*) 'COUNT'
     FROM ECOLI_DATA
-    WHERE BIN(GENOTYPE) & b'0111'= 5 
-         or BIN(GENOTYPE) & b'0111'= 4
-         or BIN(GENOTYPE) & b'0111'= 1; 
+    WHERE 
+          BIN(GENOTYPE) & b'0110'= 4
+         or BIN(GENOTYPE) & b'0011'= 1;

9b7153941f98a5e4f4b9906bd1ab764f4d5f28c1
RG <117524772+ziguin0925@users.noreply.github.com>
2024-06-25 18:12:08 +0900

    [level 2] Title: 기능개발, Time: 3.34 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -14,13 +14,18 @@ class Solution{
     public static int[] solution(int[] progresses, int[] speeds){
         Queue<Integer> qu = new LinkedList<>();
 
+        //첫번째 작업부터 시작하여 작업완료할 수있는 날짜를 구해 큐에 추가한다.
         for(int i = 0; i < progresses.length; i++){
             qu.add((int) Math.ceil(((100.0-progresses[i])/speeds[i])));
         }
-
+        
+        //첫번째 작업완료할 수있는 날짜 Max라고 친다.
         Integer Max = qu.poll();
         List<Integer> list = new ArrayList<>();
         int count =1;
+        
+        //첫번째 작업완료할 수 있는 날짜보다 같거나 작은 배포일은 같이 배포될 수 있으므로 count를 증가 시킨다.
+        //Max보다 더 큰 날짜가 나온 경우에는 Max를 바꾸어주고 count를 1로 돌린 후 다시 비교한다.  
         while(!qu.isEmpty()){
             if( Max < qu.peek()){
                 Max=qu.poll();
@@ -31,6 +36,7 @@ class Solution{
                 count++;
             }
         }
+        //마지막 count 추가
         list.add(count);
         return list.stream().mapToInt(Integer::intValue).toArray();
     }

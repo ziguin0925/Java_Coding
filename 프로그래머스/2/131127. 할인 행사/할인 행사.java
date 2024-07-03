12efb80743f42c91dbe7d9d5e8700538966e9090
RG <117524772+ziguin0925@users.noreply.github.com>
2024-07-03 10:39:04 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+class Solution {
+       static public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        for(int i=0;i<discount.length-9;i++) {
+            int j;
+            for(j=0;j<want.length;j++) {
+                int check=0;
+                for(int k=i;k<i+10;k++) {
+                    if(discount[k].equals(want[j])) check++;
+                }        
+                if(check<number[j]) {
+                     break;
+                }
+            }
+           if(j==want.length) answer ++;
+        }
+        return answer;
+    }
+    public int solution2(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+
+            HashMap<String, Integer> map = new HashMap<>();
+            HashSet<String> set = new HashSet<>();
+
+            for(int i =0; i<number.length; i++){
+                // map.put(want[i], number[i]);
+                set.add(want[i]);
+            }
+
+            for(int i =0; i<discount.length-9; i++){
+                //현재 discount품목이 want에 포함이 된다면
+                if(set.contains(discount[i])){
+                    map.put(discount[i],1);
+
+                    //현재 discount 품목 다음부터 9개 읽기.
+                    for(int j =1; j<10;j++){
+                        if(!set.contains(discount[i+j])){
+                            //i+j번째 discount 품목이 set에 없으면 map 초기화
+                            map.clear();
+                            break;
+                        }
+
+                        if(!map.containsKey(discount[i+j])){
+                            map.put(discount[i+j],1);
+                        }else{
+                            map.put(discount[i+j], map.get(discount[i+j])+1);
+                        }
+
+                        if(j==9){
+                            for(int k=0;k<number.length;k++){
+                                if(map.get(want[k])!=number[k]){
+                                    break;
+                                }
+                                if(k==number.length-1){
+                                    answer++;
+                                }
+                            }
+                            map.clear();
+                        }
+                    }
+                }
+            }
+            return answer;
+    }
+}

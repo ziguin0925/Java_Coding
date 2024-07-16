4421ea50a0a550fced04b48335c6c872b27aef17
RG <117524772+ziguin0925@users.noreply.github.com>
2024-07-16 17:23:41 +0900

    [level 2] Title: 할인 행사, Time: 82.26 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,7 +2,34 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 class Solution {
-       static public int solution(String[] want, int[] number, String[] discount) {
+    
+        public static int solution(String[] want, int[] number, String[] discount){
+            int answer = 0;
+
+            HashMap<String, Integer> map = new HashMap<>();
+
+            for(int i =0; i<number.length; i++){
+                map.put(want[i], number[i]);
+            }
+
+            for(int i =0; i<discount.length-9; i++) {
+                HashMap<String, Integer> disMap=  new HashMap<>();
+
+                for(int j =i; j<i+10; j++) {
+                    if(!disMap.containsKey(discount[j])){
+                        disMap.put(discount[j],1);
+                    }else{
+                        disMap.put(discount[j], disMap.get(discount[j])+1);
+                    }
+                }
+                if(disMap.equals(map)) answer++;
+
+            }
+            return answer;
+        }
+    
+    /*
+       static public int solution3(String[] want, int[] number, String[] discount) {
         int answer = 0;
         for(int i=0;i<discount.length-9;i++) {
             int j;
@@ -19,6 +46,7 @@ class Solution {
         }
         return answer;
     }
+    
     public int solution2(String[] want, int[] number, String[] discount) {
         int answer = 0;
 
@@ -30,7 +58,7 @@ class Solution {
                 set.add(want[i]);
             }
 
-            for(int i =0; i<discount.length-9; i++){
+            for(int i =0; i<discount.length()-9; i++){
                 //현재 discount품목이 want에 포함이 된다면
                 if(set.contains(discount[i])){
                     map.put(discount[i],1);
@@ -65,4 +93,5 @@ class Solution {
             }
             return answer;
     }
+    */
 }

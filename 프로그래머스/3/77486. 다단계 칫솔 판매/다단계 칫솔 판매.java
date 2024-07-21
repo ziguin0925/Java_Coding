db7017d771b06e7fe8c8f6e2b2c9ae3ae49a5e80
RG <117524772+ziguin0925@users.noreply.github.com>
2024-07-21 22:46:12 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 62.62 ms, Memory: 149 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+
+
+class Solution {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = new int[enroll.length];
+
+        HashMap<String, Box> map = new HashMap<>();
+
+        for(int i = 0; i < enroll.length; i++){
+            map.put(enroll[i], new Box(referral[i]));
+        }
+
+        for(int i = 0; i < seller.length; i++){
+            cal(map ,seller[i],amount[i]*100);
+        }
+
+        for(int i = 0; i < enroll.length; i++){
+            answer[i] = map.get(enroll[i]).profit;
+        }
+
+        return answer;
+
+
+
+    }
+
+    private static void cal(HashMap<String, Box> map, String seller,int amount) {
+
+        int profit = (int) Math.ceil(amount*0.9);
+        map.get(seller).profit += profit;
+        if(map.get(seller).refer.equals("-") || (amount-profit)==0){
+            return ;
+        }else{
+            cal(map, map.get(seller).refer,amount-profit);
+        }
+    }
+}
+
+class Box{
+    String refer;
+    int amount;
+    int profit;
+
+    Box(String refer){
+        this.refer = refer;
+        this.amount = 0;
+        this.profit = 0;
+    }
+
+    @Override
+    public String toString() {
+        return profit+",";
+    }
+}
+

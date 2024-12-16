e275178d2b15313af87dc027c597e5a7ae9b2fac
RG <117524772+ziguin0925@users.noreply.github.com>
2024-12-17 01:07:35 +0900

    [Silver IV] Title: 스택 2, Time: 4480 ms, Memory: 273652 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+    
+    Stack<Integer> stack = new Stack<>();
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int num = Integer.parseInt(br.readLine());
+    for(int i =0; i<num; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        String command = st.nextToken();
+        // 정수 X를 스택에 넣는다
+        if(command.equals("1")){
+            int value = Integer.parseInt(st.nextToken());
+            stack.push(value);
+            
+        // 스택에 정수가 있다면 맨 위의 정수를 빼고 출력한다. 없다면 -1을 대신 출력한다.)
+        }else if(command.equals("2")){
+        // 스택이 비어있을 경우.
+            if(stack.isEmpty()){
+                System.out.println("-1");
+            }else{
+                int lastest = stack.pop();
+                System.out.println(lastest);
+            }
+            
+        // 스택에 들어있는 정수의 개수를 출력한다.
+        }else if(command.equals("3")){
+            System.out.println(stack.size());
+            
+        //  스택이 비어있으면 1, 아니면 0을 출력한다.
+        }else if(command.equals("4")){
+            if(stack.isEmpty()){
+                System.out.println("1");
+            }else{
+                System.out.println("0");
+            }
+            
+        // 스택에 정수가 있다면 맨 위의 정수를 출력한다. 없다면 -1을 대신 출력한다.
+        }else{
+            if(stack.isEmpty()){
+                System.out.println("-1");
+            }else{
+                System.out.println(stack.peek());
+            }
+          }
+        }
+    }
+}
